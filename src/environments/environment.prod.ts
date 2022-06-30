3dae6b29b0e12ca498b2c160b8cf76b0cddde859
Marco Riemann <91315420+misterreihmaehn@users.noreply.github.com>
2022-06-30 12:33:55 +0200

    Error for Publish the Project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,16 @@
+
+
+
 export const environment = {
-  firebase: {
-    projectId: 'ring-of-fire-8427d',
-    appId: '1:526244647091:web:81334d0d15b2b6aa3de0c6',
-    storageBucket: 'ring-of-fire-8427d.appspot.com',
-    apiKey: 'AIzaSyBQrq8aEKl2BFi5WHn8Aea_K-K3LEXAPcY',
-    authDomain: 'ring-of-fire-8427d.firebaseapp.com',
-    messagingSenderId: '526244647091',
-  },
-  production: true
-};
+    production: true,
+    firebase: {
+      projectId: 'ring-of-fire-8427d',
+      appId: '1:526244647091:web:81334d0d15b2b6aa3de0c6',
+      databaseURL: 'https://ring-of-fire-8427d-default-rtdb.europe-west1.firebasedatabase.app',
+      storageBucket: 'ring-of-fire-8427d.appspot.com',
+      locationId: 'europe-west',
+      apiKey: 'AIzaSyBQrq8aEKl2BFi5WHn8Aea_K-K3LEXAPcY',
+      authDomain: 'ring-of-fire-8427d.firebaseapp.com',
+      messagingSenderId: '526244647091',
+    }
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     projectId: 'ring-of-fire-8427d',
     appId: '1:526244647091:web:81334d0d15b2b6aa3de0c6',
+    databaseURL: 'https://ring-of-fire-8427d-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ring-of-fire-8427d.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyBQrq8aEKl2BFi5WHn8Aea_K-K3LEXAPcY',
     authDomain: 'ring-of-fire-8427d.firebaseapp.com',
     messagingSenderId: '526244647091',
